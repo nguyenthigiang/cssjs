0706813d7dccb0dc20c24ca7d7b2f675db05e1c5
nguyenthigiang <nguyen.giang982@gmail.com>
2016-06-15 21:42:22 +0800

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='htp://faceseo.vn' target='_blank'><img src='http://faceseo.vn/images/banner-linh-nguyen.gif' width='500px'/>Đào tạo seo chuyên sâu</a>. Học tại nhà Linh Nguyễn. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://remcuamansaotrongtin.blogspot.com/' target='_blank'><img src='http://remcuamansao.com/userfiles/Image/News/REM-CUON.2.jpg' width='500px'/>Rèm cửa màn sáo đẹp</a>. Học tại nhà Linh Nguyễn. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
